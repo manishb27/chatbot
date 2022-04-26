d920b7ebe4967d2e6e92f67c8f3302ff23067e44
Manish <manish.babbar27@gmail.com>
2022-04-26 15:00:36 +0530

    some changes

--- actions/actions.py
+++ actions/actions.py
@@ -13,15 +13,20 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHelloWorld(Action):
+class ActionGetInterest(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_get_interest"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Hello World!")
+        # storing the list of interest extracted from user input
+        interst_list =  next(tracker.get_latest_entity_value("interest"), None)
+
+        msg = f'There are your interests? {', '.join(interst_list)}'
+
+        dispatcher.utter_message(text=msg)
 
         return []
